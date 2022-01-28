d1f111585f8f7789033da3db7595379a9cc95dfc
Alaina Thompson <athompson9@hawk.iit.edu>
2022-01-28 12:35:46 -0600

    Quadratic Formula Error Problems

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,169 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var quadraticModel = QuadraticEq()
+    @State var aString = "1.0"
+    @State var bString = "2.0"
+    @State var cString = "3.0"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text("a Value")
+                .padding(.top,25)
+                .padding(.bottom, 0)
+            TextField("Enter a value", text: $aString, onCommit: {Task.init {await self.calculatePosQuad(); await self.calculateNegQuad(); await self.calculatePrimePosQuad(); await self.calculatePrimeNegQuad()}})
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom, 30)
+                    
+            Text("b Value")
+                .padding(.top,25)
+                .padding(.bottom, 0)
+            TextField("Enter b value", text: $bString, onCommit: {Task.init {await self.calculatePosQuad(); await self.calculateNegQuad(); await self.calculatePrimePosQuad(); await self.calculatePrimeNegQuad()}})
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom, 30)
+                    
+            
+            Text("c Value")
+                .padding(.top,25)
+                .padding(.bottom, 0)
+            TextField("Enter c value", text: $cString, onCommit: {Task.init {await self.calculatePosQuad(); await self.calculateNegQuad(); await self.calculatePrimePosQuad(); await self.calculatePrimeNegQuad()}})
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom, 30)
+            
+            
+            
+            
+            HStack {
+                VStack{
+            Text("X value positive")
+                .padding(.bottom, 0)
+            TextField("", text: $quadraticModel.xPosText)
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+            
+                    
+            Text("X value Negative")
+                .padding(.bottom, 0)
+            TextField("", text: $quadraticModel.xNegText)
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+                    
+            Text("X prime value Postive")
+                .padding(.bottom, 0)
+            TextField("", text: $quadraticModel.xPrimePosText)
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+                    
+                    
+            Text("X prime value Negative")
+                .padding(.bottom, 0)
+            TextField("", text: $quadraticModel.xPrimeNegText)
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+                    
+                    
+                    
+        }
+        VStack{
+            Text("X value Positive")
+                .padding(.bottom, 0)
+            Text("\(quadraticModel.posQuad, specifier: "%.2f")")
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+                
+            
+            Text("X value Negative")
+                .padding(.bottom, 0)
+            Text("\(quadraticModel.negQuad, specifier: "%.2f")")
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+            
+            
+            Text("X prime value Positive")
+                .padding(.bottom, 0)
+            Text("\(quadraticModel.primePosQuad, specifier: "%.2f")")
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+            
+            
+            Text("X prime value Negative")
+                .padding(.bottom, 0)
+            Text("\(quadraticModel.primeNegQuad, specifier: "%.2f")")
+                .padding(.horizontal)
+                .frame(width: 100)
+                .padding(.top, 0)
+                .padding(.bottom,30)
+            
+            
+            
+            
+            
+            
+            }
+        
+                    
+                    
+                    
+                    
+                    
+
+                
+                
+                
+                
+            }
+            
+            Button("Calculate", action: {Task.init { await self.calculateQuadratic(); await self.calculateQuadratic()}})
+                .padding(.bottom)
+                .padding()
+                .disabled(quadraticModel.enableButton == false)
+           
+            
+            
+        }
+        
     }
+        
+    func calculateQuadratic() async {
+        
+        quadraticModel.setButtonEnable(state: false)
+        
+        let _ : Bool = await quadraticModel.initWithQuad(a: Double(aString)!, b: Double(bString)!, c: Double(cString)!)
+        
+        
+
 }
+    
+    
+}
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+    }
+
---
+++ Shared/Quadratic Equation.swift
@@ -0,0 +1,222 @@
+//
+//  Quadratic Equation.swift
+//  Homework 2
+//
+//  Created by Alaina Thompson on 1/28/22.
+//
+
+import SwiftUI
+
+class QuadraticEq: NSObject,ObservableObject {
+
+    var a = 0.0
+    var b = 0.0
+    var c = 0.0
+    @Published var xPos = 0.0
+    @Published var xNeg = 0.0
+    @Published var xPrimeNeg = 0.0
+    @Published var xPrimePos = 0.0
+    @Published var posQuad = 0.0
+    @Published var negQuad = 0.0
+    @Published var primePosQuad = 0.0
+    @Published var primeNegQuad = 0.0
+    @Published var aString = "1.0"
+    @Published var bString = "2.0"
+    @Published var cString = "3.0"
+    @Published var xPosText = ""
+    @Published var xNegText = ""
+    @Published var xPrimePosText = ""
+    @Published var xPrimeNegText = ""
+    @Published var enableButton = true
+    
+    
+    
+    
+    func initWithQuad(a: Double, b: Double, c: Double) async -> Bool {
+        
+            
+        let _ = await withTaskGroup(of:  Void.self) { taskGroup in
+                
+        
+            
+            taskGroup.addTask { let _ = await self.calculatePosQuad(a: self.a, b: self.b, c: self.c)}
+            taskGroup.addTask { let _ = await self.calculateNegQuad(a: self.a, b: self.b, c: self.c)}
+            taskGroup.addTask { let _ = await self.calculatePrimeNegQuad(a: self.a, b: self.b, c: self.c)}
+            taskGroup.addTask { let _ = await self.calculatePrimePosQuad(a: self.a, b: self.b, c: self.c)}
+            
+        }
+            
+            await setButtonEnable(state: true)
+                                                 
+       
+        
+
+        return true
+
+        }
+    
+    
+    
+        func calculatePosQuad(a: Double, b: Double, c: Double) async -> Double {
+                   
+                   //posQuadratic = (-b + squrt(((pow(b, 2)) - (4*a*c)), 2))/(2*a)
+                   
+                   let root = sqrt(((pow(b, 2)) - (4*a*c)))
+                   let calculatedPosQuadratic = (-b + root)/(2*a)
+                   let newXPosText = String(format: "%7.5f", calculatedPosQuadratic)
+                   
+                   await updateXPos(xPosTextString: newXPosText)
+                   await newXPosValue(xPosValue: calculatedPosQuadratic)
+                   
+                   return calculatedPosQuadratic
+                   
+               }
+        
+        
+        func calculateNegQuad(a: Double, b: Double, c: Double) async -> Double {
+                   
+                   //negQuadratic = (-b - squrt(((pow(b, 2)) - (4*a*c)), 2))/(2*a)
+                   
+                   let root = sqrt(((pow(b, 2)) - (4*a*c)))
+                   let calculatedNegQuadratic = (-b - root)/(2*a)
+                   let newXNegText = String(format: "%7.5f", calculatedNegQuadratic)
+                   
+                   await updateXNeg(xNegTextString: newXNegText)
+                   await newXNegValue(xNegValue: calculatedNegQuadratic)
+                   
+                   return calculatedNegQuadratic
+                   
+               }
+        
+        func calculatePrimePosQuad(a: Double, b: Double, c: Double) async -> Double {
+                   
+                   //negQuadratic = (-(2*c))/(b + sqrt((pow(b, 2)) - (4*a*c)))
+                   
+                   
+                   let calculatedPrimePosQuadratic = (-(2*c))/(b + sqrt((pow(b, 2)) - (4*a*c)))
+                   let newXPrimePosText = String(format: "%7.5f", calculatedPrimePosQuadratic)
+                   
+                   await updateXPrimePos(xPrimePosTextString: newXPrimePosText)
+                   await newXPrimePosValue(xPrimePosValue: calculatedPrimePosQuadratic)
+                   
+                   return calculatedPrimePosQuadratic
+                   
+               }
+        
+        
+        func calculatePrimeNegQuad(a: Double, b: Double, c: Double) async -> Double {
+                   
+                   //negQuadratic = (-(2*c))/(b - sqrt((pow(b, 2)) - (4*a*c)))
+                   
+                   
+                   let calculatedPrimeNegQuadratic = (-(2*c))/(b - sqrt((pow(b, 2)) - (4*a*c)))
+                   let newXPrimeNegText = String(format: "%7.5f", calculatedPrimeNegQuadratic)
+                   
+                   await updateXPrimeNeg(xPrimeNegTextString: newXPrimeNegText)
+                   await newXPrimeNegValue(xPrimeNegValue: calculatedPrimeNegQuadratic)
+                   
+                   return calculatedPrimeNegQuadratic
+                   
+               }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+          @MainActor func setButtonEnable(state: Bool){
+                 
+                 
+                 if state {
+                     
+                     Task.init {
+                         await MainActor.run {
+                             
+                             
+                             self.enableButton = true
+                         }
+                     }
+                     
+                     
+                         
+                 }
+                 else{
+                     
+                     Task.init {
+                         await MainActor.run {
+                             
+                             
+                             self.enableButton = false
+                         }
+                     }
+                         
+                 }
+                 
+             }
+             
+            
+             @MainActor func updateXPos(xPosTextString: String){
+                 
+                 xPosText = xPosTextString
+                 
+             }
+             
+             @MainActor func newXPosValue(xPosValue: Double){
+                 
+                 self.xPos = xPosValue
+                 
+             }
+    
+             @MainActor func newXNegValue(xNegValue: Double){
+                 
+                 self.xNeg = xNegValue
+                 
+             }
+             
+            
+             @MainActor func updateXNeg(xNegTextString:String){
+                 
+                 xNegText = xNegTextString
+                 
+                 
+             }
+        
+        @MainActor func updateXPrimePos(xPrimePosTextString: String){
+            
+            xPrimePosText = xPrimePosTextString
+            
+        }
+        
+        @MainActor func newXPrimePosValue(xPrimePosValue: Double){
+            
+            self.xPrimePos = xPrimePosValue
+            
+        }
+        
+        @MainActor func newXPrimeNegValue(xPrimeNegValue: Double){
+            
+            self.xPrimeNeg = xPrimeNegValue
+            
+        }
+        
+       
+        @MainActor func updateXPrimeNeg(xPrimeNegTextString:String){
+            
+            xPrimeNegText = xPrimeNegTextString
+            
+            
+        }
+        
+    }
+        
+        
+        
+}
+        
+        
+}
